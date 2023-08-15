c8ed925a4dac704aab93f5a8c7573760dc9ee150
1dreams1 <81896875+1dreams1@users.noreply.github.com>
2023-08-15 09:35:25 +0800

    Update and rename train.py to main.py

--- main.py
+++ main.py
@@ -10,15 +10,20 @@ class RunCmd(object):
         subprocess.call(self.cmd, shell=True)
 
 
-# Train the model
-for i in range(1):
+# Train and test the model
+for i in range(2):
         print('train：',i)
         a = RunCmd()
         a.cmd_run('python FMNet.py \
             --posi=./GraphProt_CLIP_sequences/ZC3H7B_Baltz2012.train.positives.fa \
             --nega=./GraphProt_CLIP_sequences/ZC3H7B_Baltz2012.train.negatives.fa\
             --train=True --n_epochs=50')
-
+    for j in range(10):
+      a = RunCmd()
+      a.cmd_run('python FMNet.py \
+              --testfile=./GraphProt_CLIP_sequences/ZC3H7B_Baltz2012.ls.positives.fa \
+              --nega=./GraphProt_CLIP_sequences/ZC3H7B_Baltz2012.ls.negatives.fa\
+              --predict=True')
 
 
 
